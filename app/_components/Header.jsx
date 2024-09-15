7947d2b4f840cd02f65d49438ad236512a24f005
Sudhanshu Kumar <sudhanshukumar2207@gmail.com>
2024-09-15 10:51:52 +0530

    initial commit

---
+++ app/_components/Header.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      {/* <Button>i am header</Button> */}
+      header
+    </div>
+  )
+}
